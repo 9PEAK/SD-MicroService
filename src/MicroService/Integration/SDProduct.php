c9e341eb15aa0251efcd328e92ccbe5269ec34bf
Kane <52653784@qq.com>
2018-07-04 17:31:37 +0800

    SD-MS Start

--- src/MicroService/Integration/SDProduct.php
+++ src/MicroService/Integration/SDProduct.php
@@ -6,10 +6,16 @@ class SDProduct extends \Peak\MicroService\Core {
 
 	use Base;
 
-	protected static function searchProduct (array $param, $query=null, $method='post')
-	{
-		return [
 
+	protected static function searchProduct (array $param /*, $query=null, $method='post'*/)
+	{
+		return @ [
+			'id' => $param['id'],
+			'exceptId' => $param['exceptId'],
+			'mfn' => $param['mfn'],
+			'exceptMfn' => $param['exceptMfn'],
+			'name' => $param['name'],
+			'categoryId' => $param['categoryId'],
 		];
 	}
 
